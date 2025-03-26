ac03e19de28aee2d3dc1fab881b1068e15b3c6f7
Victor Camarena <vcamarena79@gmail.com>
2025-03-25 18:59:59 -0600

    Parsing a Command-Line Flags (3)

--- my-cli/bin/cmd.js
+++ my-cli/bin/cmd.js
@@ -3,6 +3,12 @@ import { got } from "got";
 import {Command} from "commander";
 // Create a new Command Program
 const program = new Command();
+const API = "http://localhost:3000";
+
+// Log the usage of the command to the console
+const usage = (msg = "Back office for My App") => {
+    console.log(`\n${msg}\n`);
+};
 
 // Create a new Program
 program
@@ -12,13 +18,40 @@ program
 // Parse the arguments from process.argv
 program.parse();
 
-const API = "http://localhost:3000";
+// Update the order with the given ID
+async function updateItem(id, amount) {
+    usage(`Updating order ${id} with amount ${amount}`)
+    try {
+	if (isNaN(+amount)) {
+	    usage("Error: <AMOUNT> must be a number");
+	    process.exit(1);
+	}
+	// Use GOT to make a POST request to te API
+	await got.post(`${API}/orders/${id}`, {
+	    json: { amount: +amount },
+	});
+	// log the result to the console
+	usage(`Order ${id} updated with amount ${amount}`);
+    } catch (err) {
+	// If there is an error, log it to the console and exit
+	console.error(err.message);
+	process.exit(1);
+    }
+}
 
-// Log the usage of the m¿commando to the console
-const usage = (msg = "Back office for My App") => {
-    console.log(`\n${msg}\n`);
-    console.log("Usage: cmd <ID> <AMOUNT>");
-};
+// Create a command for adding a new order
+program
+// Set the command name
+    .command("update")
+// Set the argument ID to be required
+    .argument("<ID>", "Order ID")
+// Set the argument AMOUNT to be required
+    .argument("<AMOUNT>", "Order Amount")
+// Set the action to be excecuted when the command is run
+    .action(async (id, amount) => await updateItem(id, amount));
+
+// Parse the arguments from process.argv
+program.parse();
 
 // Get the arguments from the command line
 const argv = process.argv.slice(2);
